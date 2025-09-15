d5e96e45bde828c37b126e8f50f041d3fa0174b6
ogguy <yannokra01@gmail.com>
2025-09-15 08:44:43 +0000

    Mise a jour

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
               <GetMailForm />
 
               <motion.a
-                href={"/src/assets/CATALOGUE KREOB VF.pdf"}
+                href={"/CATALOGUE KREOB VF.pdf"}
                 className=" h-9 px-4 py-2 has-[>svg]:px-3 items-center gap-2 bg-primary text-primary-foreground shadow-sm hover:bg-primary/90 rounded-md"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
